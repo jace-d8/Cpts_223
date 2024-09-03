07fc4ee27121cfd59714a373374f84ea6c30d3bb
jace-d8 <142870592+jace-d8@users.noreply.github.com>
2024-09-02 18:34:46 -0700

    Starter code updated

---
+++ main.cpp
@@ -0,0 +1,76 @@
+/*
+ADVANTAGES/DISADVANTAGES LINKED LIST
+
+ADVANTAGES/DISADVANTAGES ARRAY
+*/
+
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "linkedlist.cpp"
+
+using namespace std;
+
+void populate(Linked_List<string, string> *mainlist) // Read CSV and input data into a linked list
+{
+    // ====================
+    // ======= code =======
+    // ====================
+}
+
+int main()
+{
+
+    // =====================
+    // ===== some code =====
+    // =====================
+
+    while (true)
+    {
+        cout << "Please select an option listed below:\n";
+        cout << "1. Game Rules\n2. Play Game\n3. Load Previous Game\n4. Add Command\n5. Remove Command\n6. Exit\n";
+        int selection;
+        cin >> selection;
+
+        if (selection == 2) // Play Game
+        {
+            // =====================
+            // ===== some code =====
+            // =====================
+        }
+        else if (selection == 1) // Game Rules
+        {
+            // =====================
+            // ===== some code =====
+            // =====================
+        }
+        else if (selection == 3) // Load Previous Game
+        {
+            // =====================
+            // ===== some code =====
+            // =====================
+        }
+        else if (selection == 4) // Add Command
+        {
+            // =====================
+            // ===== some code =====
+            // =====================
+        }
+        else if (selection == 5) // Remove Command
+        {
+            // =====================
+            // ===== some code =====
+            // =====================
+        }
+        else if (selection == 6) // Exit
+        {
+            // =====================
+            // ===== some code =====
+            // =====================
+            return 0;
+        }
+    }
+}
+
