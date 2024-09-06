12dbb3d34daf2ab9ead8457e36f1941c41299edf
jace-d8 <142870592+jace-d8@users.noreply.github.com>
2024-09-06 14:57:25 -0700

    Remove and insert at front implemented, finished destructor

--- linkedlist.h
+++ linkedlist.h
@@ -11,9 +11,15 @@ public:
     Linked_List(Node<t, d> new_head): _head(new_head){}
     ~Linked_List();
 
+    bool removeFromFront();
+
+    void insert_at_front(Node<t, d> new_node);
+
     void open_file(const std::string& file_name);
 
     void upload_file();
+
+    bool is_empty();
 
 private:
     Node<t, d> * _head; // No clue why their needs to be a two type template... the command and description
@@ -23,10 +29,37 @@ private:
 template<typename t, typename d>
 Linked_List<t, d>::~Linked_List()
 {
-    delete _head; // actually make this later
+    while(!is_empty())
+    {
+        removeFromFront();
+    }
+}
+template<typename t, typename d>
+bool Linked_List<t,d >::removeFromFront()
+{
+    const Node<t, d>* tempNode = _head;
+    if(!this->is_empty())
+    {
+        _head = _head->_next;
+        delete tempNode;
+        return true;
+    }
+    return false;
 }
 
-
+template<typename t, typename d>
+void Linked_List<t, d>::insert_at_front(Node<t, d> new_node)
+{
+    Node<t, d>* tmp = _head;
+    if(this->is_empty())
+    {
+        _head = new_node;
+    }else
+    {
+        _head = new_node;
+        _head->next = tmp;
+    }
+}
 
 template<typename t, typename d>
 void Linked_List<t, d>::open_file(const std::string& file_name)
@@ -34,8 +67,7 @@ void Linked_List<t, d>::open_file(const std::string& file_name)
     if(_input_stream.is_open())
     {
         cout<<"File not found"<<endl;
-    }
-    else
+    }else
     {
         _input_stream.open(file_name);
 
@@ -45,14 +77,20 @@ void Linked_List<t, d>::open_file(const std::string& file_name)
 template<typename t, typename d>
 void Linked_List<t,d>::upload_file()
 {
-    Node<t, d>* current = _head;
-    char buffer[99] = "";
+    char cmnd[99] = "", ds[99] = "";
+    Node<t, d>* tmp = _head;
     while(_input_stream.good())
     {
-        _input_stream.getline(buffer, 99, ',');
-        current->set_command(buffer);
-        _input_stream.getline(buffer, 99);
-        current->set_data(buffer);
-
+        _input_stream.getline(cmnd, 99, ',');
+        _input_stream.getline(ds, 99);
+        insert_at_front(new Node<t, d>(cmnd, ds));
     }
+}
+
+template<typename t, typename d>
+bool Linked_List<t,d>::is_empty()
+{
+    if(_head == nullptr)
+        return true;
+    return false;
 }
