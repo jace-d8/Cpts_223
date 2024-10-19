19b10654b4de332c5219fb4404e0bcbab9f12c27
jace-d8 <142870592+jace-d8@users.noreply.github.com>
2024-10-18 20:15:45 -0700

    Setup MA_3

--- main.cpp
+++ main.cpp
@@ -1,30 +1,141 @@
-// This code was taken from https://www.techiedelight.com/queue-implementation-cpp/
-// The code has been modified from the original to provide opportunities to learn
-
-#include <queue>
-
-#include "testQueue.h"
-
-// define default capacity of the queue
+#include <map>
+#include "generateUsers.hpp"
+using namespace std;
 
 
-// main function
+map<string, User> buildMapByUserName(vector<User> users) {
+    map<string, User> usersMap;
+    
+    cout << "TODO" << endl;
+
+    return usersMap;
+}
+
+
+map<string, User> buildMapByEmail(vector<User> users) {
+    map<string, User> usersMap;
+    
+    cout << "TODO" << endl;
+
+    return usersMap;
+}
+
+
+void printMap(map<string, User> aMap) {
+    cout << "TODO" << endl;
+}
+
+
+bool isMapSorted(map<string, User> aMap) {
+    if ( aMap.empty() )
+        return true;
+    
+    cout << "TODO" << endl;
+
+    return false;
+}
+
+
+bool testSearchByKey(map<string, User> aMap, string keyToSearch) {
+    cout << "TODO" << endl;
+    return false;
+}
+
+
+bool testDeleteByKey(map<string, User> aMap, string keyToDelete) {
+    cout << "TODO" << endl;
+    return false;
+}
+
+
+void printActiveUsers(map<string, User> aMap) {
+    int activeThreshold = 800;
+    cout << "TODO" << endl;
+}
+
+
+void printMostPopularCategory(map<string, User> aMap) {    
+    cout << "TODO" << endl;
+}
+
+
 int main()
 {
-    // call your test functions here!
+    int numUsers = 10;
+    vector<User> users = generateUsers(numUsers);
 
-    testQueue testQueue;
-    testQueue.test_dequeue();
-    testQueue.test_enqueue();
-    testQueue.test_peek();
-    testQueue.test_peekEmpty();
-    testQueue.test_dequeueEmpty();
-    testQueue.test_enqueueFull();
 
-    testQueue.test_isEmpty();
-    testQueue.test_isFull();
-    testQueue.test_size();
+    cout << "Build map with username as key" << endl;
+    map<string, User> mapByUserName = buildMapByUserName(users);
+    if ( mapByUserName.size() == numUsers )
+        cout << "  Built successfully." << endl << endl;
+    else
+        cout << "  Built unsuccessfully." << endl << endl;
+
+
+    cout << "Print \"mapByUserName\" map:" << endl;
+    printMap(mapByUserName);
+    cout << endl;
+
+
+    string keyToSearch = "smith55";
+    cout << "Search by key: mapByUserName[\"" << keyToSearch << "\"]" << endl;
+    if ( testSearchByKey(mapByUserName, keyToSearch) )
+        cout << "  Search successfully." << endl << endl;
+    else
+        cout << "  Search unsuccessfully." << endl << endl;
+
+
+    string keyToDelete = "smith55";
+    cout << "Delete by key: \"" << keyToDelete << "\"" << endl;
+    if ( testDeleteByKey(mapByUserName, keyToDelete) )
+        cout << "  Delete successfully." << endl << endl;
+    else
+        cout << "  Delete unsuccessfully." << endl << endl;
+
+
+    cout << "Test if map's key is sorted" << endl;
+    if ( isMapSorted(mapByUserName) )
+        cout << "  Order test passed!" << endl << endl;
+    else
+        cout << "  Order test failed!" << endl << endl;
+
+
+    cout << "Print usernames with more than 800 tweets:" << endl;
+    printActiveUsers(mapByUserName);
+    cout << endl;
+
+    cout << "Print the most popular category" << endl;
+    printMostPopularCategory(mapByUserName);
+    cout << endl;
+
+
+    cout << " ============================================================================== " << endl << endl;
+
+
+    cout << "Build map with username as key" << endl;
+    map<string, User> mapByEmail = buildMapByEmail(users);
+    if ( mapByEmail.size() == numUsers )
+        cout << "  Built successfully." << endl << endl;
+    else
+        cout << "  Built unsuccessfully." << endl << endl;
+    
+
+    keyToSearch = "kat@gmail.com";
+    cout << "Search by key: mapByEmail[\"" << keyToSearch << "\"]" << endl;
+    if ( testSearchByKey(mapByEmail, keyToSearch) )
+        cout << "  Search successfully." << endl << endl;
+    else
+        cout << "  Search unsuccessfully." << endl << endl;
+
+
+    keyToDelete = "kat@gmail.com";
+    cout << "Delete by key: \"" << keyToDelete << "\"" << endl;
+    if ( testDeleteByKey(mapByEmail, keyToDelete) )
+        cout << "  Delete successfully." << endl << endl;
+    else
+        cout << "  Delete unsuccessfully." << endl << endl;
+
 
     return 0;
 }
-
